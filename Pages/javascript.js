2a81d9d621720c5dc6a775fb6eef4072f10e3c9c
djhall22 <djhallba@gmail.com>
2023-01-23 10:56:18 -0500

    ternary rock paper scissors

--- Pages/javascript.js
+++ Pages/javascript.js
@@ -1,46 +1,24 @@
+//Conditionals: ternary operator practice
+
+let soup;
+let isCustomerBanned = false;
+let soupAccess = isCustomerBanned
+  ? "NO SOUP FOR YOU!"
+  : soup
+  ? `Yes, we have ${soup} today`
+  : "sorry, no soup today";
+console.log(soupAccess);
+
 let playerOne = "paper";
 let computer = "scissors";
-
-switch (playerOne) {
-  case computer:
-    console.log("Tie Game!");
-    break;
-
-  case "rock":
-    if (computer === "paper") {
-      console.log("computer wins");
-    } else {
-      console.log("playerOne Wins!");
-    }
-    break;
-  case "paper":
-    if (computer === "scissors") {
-      console.log("computer wins");
-    } else {
-      console.log("playerOne Wins!");
-    }
-    break;
-  default:
-    if (computer === "rock") {
-      console.log("computer wins!");
-    } else {
-      console.log("playerOne wins!");
-    }
-}
-
-switch (Math.floor(Math.random() * 5 + 1)) {
-  case 1:
-    console.log(1);
-    break;
-
-  case 2:
-    console.log(2);
-    break;
-
-  case 3:
-    console.log(3);
-    break;
-
-  default:
-    console.log("no match");
-}
+let result =
+  playerOne === computer
+    ? "Tie Game"
+    : playerOne === "rock" && computer === "scissors"
+    ? "playerOne wins!"
+    : playerOne === "paper" && computer === "scissors"
+    ? "computer wins!"
+    : playerOne === "scissors" && computer === "paper"
+    ? "playerOne Wins!"
+    : "playerOne wins";
+console.log(result);
